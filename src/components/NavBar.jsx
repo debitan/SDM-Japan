6c5db650dd374dc44e3bbbc21ec4c0fff805428a
David Matthews <dwm503@gmail.com>
2020-01-26 17:51:30 +0000

    make navbar show you where you are

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,6 +36,7 @@ const StyledNavDropdown = styled(NavDropdown)`
         color: black !important;
         padding: 0 !important;
         margin: 15px 10px;
+        border-bottom: ${props => props.links.includes(props.location) ? '2px solid black' : 'none'};
     }
 
     > div {
@@ -45,7 +46,7 @@ const StyledNavDropdown = styled(NavDropdown)`
         font-size: 18px;
         margin: 0;
         border-radius: 0;
-        width: 8gi0%;
+        width: 80%;
         min-width: 240px;
         left: auto;
     }
@@ -85,6 +86,10 @@ const Phone = styled('span')`
     }
 `
 
+const DropdownItem = styled(NavDropdown.Item)`
+    background-color: ${props => props.location === props.href ? 'white' : 'none'} !important;
+`
+
 function NavBar() {
     const { sanityHeader } = useStaticQuery(graphql`
         query headerQuery {
@@ -94,6 +99,10 @@ function NavBar() {
         }
     `)
 
+    const aboutUsLinks = ["/who-we-are/", "/team/", "/reports/", "/publications/"]
+    const activitiesLinks = ["/programme/", "/workshops/", "/talking-mat/", "/collaboration/"]
+    const whatYouCanDoLinks = ["/request-a-speaker/", "/events/", "/become-a-member/", "/stay-in-touch/"]
+
     return (
         // <MyContext.Consumer>
         //     {context =>
@@ -102,12 +111,12 @@ function NavBar() {
                 return (
                     <>
                         <Container>
-                                <LogoWrapper>
-                                    <Link href='/'>
-                                        <StyledImage src={logo} alt='SDMーJapanのロゴ' />
-                                    </Link>
-                                    <Phone>{sanityHeader.phone}</Phone>
-                                </LogoWrapper>
+                            <LogoWrapper>
+                                <Link href='/'>
+                                    <StyledImage src={logo} alt='SDMーJapanのロゴ' />
+                                </Link>
+                                <Phone>{sanityHeader.phone}</Phone>
+                            </LogoWrapper>
                         </Container>
                         <StyledNavbar expand="lg">
                             <Container>
@@ -115,26 +124,26 @@ function NavBar() {
                                 <Navbar.Collapse id="collapsible-navbar-nav">
                                     <Nav className="mr-auto ml-auto">
                                         <StyledLink href="/" location={location.pathname}>Home</StyledLink>
-                                        <StyledNavDropdown title="私たちについて" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/who-we-are">私たちについて</NavDropdown.Item>
-                                            <NavDropdown.Item href="/team">チーム</NavDropdown.Item>
-                                            <NavDropdown.Item href="/reports">活動報告書</NavDropdown.Item>
-                                            <NavDropdown.Item href="/publications">出版・メディア</NavDropdown.Item>
+                                        <StyledNavDropdown title="私たちについて" id="basic-nav-dropdown" location={location.pathname} links={aboutUsLinks}>
+                                            <DropdownItem href="/who-we-are/" location={location.pathname}>私たちについて</DropdownItem>
+                                            <DropdownItem href="/team/" location={location.pathname}>チーム</DropdownItem>
+                                            <DropdownItem href="/reports/" location={location.pathname}>活動報告書</DropdownItem>
+                                            <DropdownItem href="/publications/" location={location.pathname}>出版・メディア</DropdownItem>
                                         </StyledNavDropdown>
-                                        <StyledNavDropdown title="私たちの活動" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="#action/3.1">意思決定支援実践のための総合プログラム開発</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.2">研修 ・ ワークショップ</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.3">トーキングマット</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.3">他団体への協賛</NavDropdown.Item>
+                                        <StyledNavDropdown title="私たちの活動" id="basic-nav-dropdown" location={location.pathname} links={activitiesLinks}>
+                                            <DropdownItem href="#action/3.1" location={location.pathname}>意思決定支援実践のための総合プログラム開発</DropdownItem>
+                                            <DropdownItem href="#action/3.2" location={location.pathname}>研修 ・ ワークショップ</DropdownItem>
+                                            <DropdownItem href="#action/3.3" location={location.pathname}>トーキングマット</DropdownItem>
+                                            <DropdownItem href="#action/3.3" location={location.pathname}>他団体への協賛</DropdownItem>
                                         </StyledNavDropdown>
-                                        <StyledNavDropdown title="あなたにできること" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="#action/3.1">講師を依頼する</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.2">イベント参加</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.3">会員になる</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.3">メーリングリスト登録・SNSフォローする</NavDropdown.Item>
+                                        <StyledNavDropdown title="あなたにできること" id="basic-nav-dropdown" location={location.pathname} links={whatYouCanDoLinks}>
+                                            <DropdownItem href="#action/3.1" location={location.pathname}>講師を依頼する</DropdownItem>
+                                            <DropdownItem href="#action/3.2" location={location.pathname}>イベント参加</DropdownItem>
+                                            <DropdownItem href="#action/3.3" location={location.pathname}>会員になる</DropdownItem>
+                                            <DropdownItem href="#action/3.3" location={location.pathname}>メーリングリスト登録・SNSフォローする</DropdownItem>
                                         </StyledNavDropdown>
-                                        <StyledLink href="/news" location={location.pathname}>ニューズ</StyledLink>
-                                        <StyledLink href="/contact" location={location.pathname}>問い合わせ</StyledLink>
+                                        <StyledLink href="/news/" location={location.pathname}>ニュース</StyledLink>
+                                        <StyledLink href="/contact/" location={location.pathname}>問い合わせ</StyledLink>
                                         <DonationLink>
                                             <StyledLink style={{ "color": "white", "borderColor": "white" }} href="/donate" location={location.pathname}>寄付する</StyledLink>
                                         </DonationLink>
