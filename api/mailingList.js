48a2a1633c8915d9d2d87ecf2f3d2911f401a186
David Matthews <dwm503@gmail.com>
2020-02-13 19:01:41 +0000

    add correct email address to serverless function

--- api/mailingList.js
+++ api/mailingList.js
@@ -8,7 +8,7 @@ export default async function(req, res) {
   } = req.body
 
   const content = {
-    to: 'index@sdm-japan.net',
+    to: 'info@sdm-japan.net',
     from: email,
     subject: `メーリングアドレス登録の依頼 From - ${email}`,
     text: `
