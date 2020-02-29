a1a4721fd79eaa44efe3a3e052690d55a937379a
David Matthews <dwm503@gmail.com>
2020-02-29 12:02:49 +0000

    add youtube object serializer

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import getYouTubeId from 'get-youtube-id'
+import YouTube from 'react-youtube'
 
 const serializers = {
     types: {
@@ -11,7 +13,12 @@ const serializers = {
             <pre data-language={props.node.language}>
                 <code>{props.node.code}</code>
             </pre>
-        )
+        ),
+        youtube: ({node}) => {
+            const { url } = node
+            const id = getYouTubeId(url)
+            return (<YouTube videoId={id} />)
+        }
     }
 }
 
