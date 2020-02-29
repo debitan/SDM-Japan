929f37f6cf783afd371903cd6a22492d4d563229
David Matthews <dwm503@gmail.com>
2020-02-29 12:48:59 +0000

    first iteration of talking mats page and improve serializer

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import getYouTubeId from 'get-youtube-id'
 import YouTube from 'react-youtube'
+import Img from 'gatsby-image'
+import { getFluidGatsbyImage } from 'gatsby-source-sanity'
+
+import ColumnWrapper from './shared/ColumnWrapper'
+
+const StyledImg = styled(Img)`
+    margin: 2rem 0;
+    width: 75%;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const StyledYouTube = styled(YouTube)`
+    margin: 2rem 0;
+    max-width: 100%;
+`
 
 const serializers = {
     types: {
@@ -17,8 +34,20 @@ const serializers = {
         youtube: ({node}) => {
             const { url } = node
             const id = getYouTubeId(url)
-            return (<YouTube videoId={id} />)
-        }
+            return (
+                <ColumnWrapper>
+                    <StyledYouTube videoId={id} />
+                </ColumnWrapper>
+            )
+        },
+        captionImage: ({node}) => {
+            const sanityConfig = {
+                projectId: "3xourkaf",
+                dataset: "production",
+            }
+
+            return (<StyledImg fluid={getFluidGatsbyImage(node.image.asset, {maxWidth: 1024}, sanityConfig)} alt={node.caption.ja} />)
+        },
     }
 }
 
