c5699d4fc8ff3bcb3f6cc85543ce8f49de1c235d
David Matthews <dwm503@gmail.com>
2020-03-17 08:00:07 +0000

    add request a speaker page
    Conflicts:
    	src/components/news/NewsArticles.jsx
    	src/pages/index.jsx
    	src/pages/news.jsx

--- src/pages/request-a-speaker.jsx
+++ src/pages/request-a-speaker.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 import BlockContent from "@sanity/block-content-to-react"
 
 import SEO from "../components/SEO"
